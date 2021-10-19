72e2fa21f5c98418fdd1abfa9e03f7c46b522306
Aldo Al triansah <aldoaltriansah7@gmail.com>
2021-10-19 14:54:19 +0700

    init

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -1,38 +1,54 @@
-<fieldset>
-    <legend>List Products</legend>
-    <form action="/products/create" method="GET">
-        <input type="submit" value="create">
-    </form>
-    <table border="1">
-        <tr>
-            <td>NO</td>
-            <td>ID</td>
-            <td>Nama Product</td>
-            <td>Jenis Product</td>
-            <td>Harga Product</td>
-            <td>Stok Product</td>
-            <td colspan="2">Action</td>
-        </tr>
-        @foreach ($products as $product)
-        <td>1</td>
-        <td>{{ $product->id }}</td>
-        <td>{{ $product->nama_product }}</td>
-        <td>{{ $product->jenis_product }}</td>
-        <td>{{ $product->harga }}</td>
-        <td>{{ $product->stok }}</td>
-        <td>
-            <form action="/products/{{ $product->id }}/edit" method="GET">
-                <input type="submit" value="Edit produk">
-            </form>
-        </td>
-        <td>
-            <form action="/products/{{ $product->id }}/delete" method="POST">
-                @method('DELETE')
-                @csrf
-                <input type="submit" value="Hapus produk">
-            </form>
-        </td>
-        @endforeach
+@extends('header')
 
-    </table>
-</fieldset>
+@section('contents')
+
+<div class="row m-5">
+    <div class="md-col-5">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-tittle fs-3 mb-3">
+                    list Data Barang
+                </div>
+                    <form action="/products/create" method="GET">
+                        <input type="submit" value="create">
+                    </form>
+                    <table class="table table-hover mt-5">
+                        <tr>
+                            <td>NO</td>
+                            <td>ID</td>
+                            <td>Nama Product</td>
+                            <td>Jenis Product</td>
+                            <td>Harga Product</td>
+                            <td>Stok Product</td>
+                            <td colspan="2">Action</td>
+                        </tr>
+
+                        @foreach ($products as $product)
+                        <td>1</td>
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->nama_product }}</td>
+                        <td>{{ $product->jenis_product }}</td>
+                        <td>{{ $product->harga }}</td>
+                        <td>{{ $product->stok }}</td>
+                        <td>
+                            <form action="/products/{{ $product->id }}/edit" method="GET">
+                                <input type="submit" value="Edit produk">
+                            </form>
+                        </td>
+                        <td>
+                            <form action="/products/{{ $product->id }}/delete" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <input type="submit" value="Hapus produk">
+                            </form>
+                        </td>
+                        @endforeach
+
+                    </table>
+
+                </fieldset>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
